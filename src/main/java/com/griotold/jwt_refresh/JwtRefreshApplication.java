2fb661d5879c93436f827cc9c578de7e7022e855
Griotold <gotjd9773@naver.com>
2025-03-23 10:55:07 +0900

    Initial commit

---
+++ src/main/java/com/griotold/jwt_refresh/JwtRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.griotold.jwt_refresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRefreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtRefreshApplication.class, args);
+	}
+
+}
